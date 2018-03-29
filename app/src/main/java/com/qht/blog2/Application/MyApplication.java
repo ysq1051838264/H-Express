8eed0bff31b4322ae639c2a1694b25120bbf2127
yangshuquan <1051838264@qq.com>
2018-03-29 10:03:59 +0800

    Initial commit

---
+++ app/src/main/java/com/qht/blog2/Application/MyApplication.java
@@ -0,0 +1,50 @@
+package com.qht.blog2.Application;
+
+import com.amitshekhar.DebugDB;
+import com.baidu.mapapi.SDKInitializer;
+import com.qht.blog2.Net.OK_LoggingInterceptor;
+import com.qht.blog2.Util.LogUtil;
+import com.qht.blog2.Util.SharePreferenceUtil;
+import com.umeng.commonsdk.UMConfigure;
+import com.zhy.http.okhttp.OkHttpUtils;
+
+import org.litepal.LitePalApplication;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * Created by QHT on 2017-02-27.
+ */
+
+public class MyApplication  extends LitePalApplication {
+
+    protected static MyApplication instance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance =this;
+        LogUtil.e(DebugDB.getAddressLog());
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        //注意该方法要再setContentView方法之前实现
+        SDKInitializer.initialize(getApplicationContext());//初始化百度地图
+        SharePreferenceUtil.initSharePreferenceUtil(getApplicationContext());
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()//init OkHttp3.0
+                .connectTimeout(8000L, TimeUnit.MILLISECONDS)
+                .readTimeout(8000L, TimeUnit.MILLISECONDS)
+                .retryOnConnectionFailure(true)
+                .addInterceptor(new OK_LoggingInterceptor())
+                //其他配置
+                .build();
+        OkHttpUtils.initClient(okHttpClient);
+
+        UMConfigure.init(this, UMConfigure.DEVICE_TYPE_PHONE, "5aba0372b27b0a063e0000f6");
+
+    }
+
+    public static MyApplication getInstance() {
+        return instance;
+    }
+}
